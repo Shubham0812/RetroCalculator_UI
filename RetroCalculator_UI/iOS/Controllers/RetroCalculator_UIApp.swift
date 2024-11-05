4ad82effdca17400770a4c1f4d7a6da6e6264326
Shubham <Shubham.singh@dailyrounds.org>
2024-11-05 17:30:04 +0530

    Add File/Folder Structure || Initialise Project

---
+++ RetroCalculator_UI/iOS/Controllers/RetroCalculator_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroCalculator_UIApp.swift
+//  RetroCalculator_UI
+//
+//  Created by Shubham on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RetroCalculator_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
